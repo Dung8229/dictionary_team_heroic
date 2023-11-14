718ddb036002f1c3657e7d2eba98087df0b379de
Dũng Quang <22028229@vnu.edu.vn>
2023-11-14 22:37:36 +0700

    Week 12
    - Combine back-end front-end work

--- src/main/java/Main/MainBoard.java
+++ src/main/java/Main/MainBoard.java
@@ -23,16 +23,14 @@ public class MainBoard implements Initializable {
     private AnchorPane bookmarkPane;
     @FXML
     private AnchorPane historyPane;
+    @FXML
+    private AnchorPane updatePane;
 
     @FXML
     private AnchorPane menuPane, blendPane, touchPane;
 
     @FXML
     private Button menu, searchButton, translateButton, savedListButton;
-    @FXML
-    private BookmarkController bookmarkController;
-    @FXML
-    private HistoryController historyController;
 
     public void setMainBoard(AnchorPane pane) {
         mainBoard.getChildren().setAll(pane);
@@ -50,26 +48,28 @@ public class MainBoard implements Initializable {
     public void OpenBookmarkPane() {
         setMainBoard(bookmarkPane);
     }
+    public void OpenUpdatePane() {
+        setMainBoard(updatePane);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             searchPane = FXMLLoader.load(MainBoard.class.getResource("/fxml/SearchPane.fxml"));
             translatePane = FXMLLoader.load(MainBoard.class.getResource("/fxml/TranslatePane.fxml"));
+            updatePane = FXMLLoader.load(MainBoard.class.getResource("/fxml/UpdatePane.fxml"));
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
             FXMLLoader loader = new FXMLLoader(MainBoard.class.getResource("/fxml/BookmarkPane.fxml"));
             bookmarkPane = loader.load();
-            bookmarkController = loader.getController();
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
             FXMLLoader loader = new FXMLLoader(MainBoard.class.getResource("/fxml/HistoryPane.fxml"));
             historyPane = loader.load();
-            historyController = loader.getController();
         } catch (Exception e) {
             e.printStackTrace();
         }
